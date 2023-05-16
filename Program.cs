20ec69ff4728458eb4857e82d47a93a7d0bb7c29
Heloisa Duarte <duarteh729@gmail.com>
2023-05-16 22:21:24 +0000

    Estrutura de repetição

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 
 var nome = Console.ReadLine();
 
@@ -15,12 +15,15 @@ var caractere = 'a';
 var data = new DateTime(1992, 1, 1);
 var agora = DateTime.Now;
 
-Console.WriteLine(agora);*/
+Console.WriteLine(agora);
 
 Console.WriteLine("Digite uma opção entre 1 e 3");
 
 var opcao = Console.ReadLine();
 
+//Estrutura de condição
+
+Console.WriteLine("Com  If");
 if(opcao == "1"){
     Console.WriteLine("Opção 1 selecionada.");
 }else if(opcao == "2"){
@@ -45,4 +48,37 @@ switch(opcao){
     default: 
         Console.WriteLine("Opção inválida");
         break;
+}
+
+//Estrutura de repetição
+
+var valores = "0 1 2 3 4 5";
+var numeros = valores.Split(' '); // Criar um array
+
+Console.WriteLine("Repetição com For");
+for(var i = 0; i < numeros.Length; i++){
+    Console.Write(numeros[i]);
+}
+
+Console.WriteLine();
+
+var contador = 0;
+
+Console.WriteLine("Com While");
+
+while(contador < numeros.Length){
+    Console.Write(numeros[contador]);
+    contador++;
+}
+
+Console.WriteLine();
+
+do{
+    Console.WriteLine("Executando com do-while");
+} while(numeroInt == 3);
+
+
+Console.WriteLine("Executando Com Foreach");
+foreach(var numero in numeros){
+    Console.WriteLine(numero);
 }
